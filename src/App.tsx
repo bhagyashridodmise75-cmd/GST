6900e64e38181859c7647c0859472e2ef7f48325
sanika <bhagyashridodmise075@gmai.com>
2026-08-10 13:44:33 +0530

    singup updated

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ export function App() {
     return (
       <LandingPage
         onGetStarted={() => setIsAuthModalOpen(true)}
-        onViewDemo={() => setCurrentView('dashboard')}
+        onViewDemo={() => setIsAuthModalOpen(true)}
       />
     );
   }
